cd4e413ba9434f38719f543ef2efb5d3c28187fb
ShadowCoder <variax20@gmail.com>
2026-02-07 00:50:37 +0000

    Quick port fix for connecting to Render for deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,4 +12,5 @@ const availabilityRouter = require("./routes/availability");
 app.use("/gigs", gigsRouter);
 app.use("/availability", availabilityRouter);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
